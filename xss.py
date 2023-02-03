3d588b05830788fd29d7b9563c30ed67f63c9cfe
Yogendra Singh <106531785+YOGENDRA-S@users.noreply.github.com>
2023-02-03 23:00:31 +0530

    Update xss.py

--- xss.py
+++ xss.py
@@ -32,3 +32,25 @@ def scan_url(url):
 if __name__ == "__main__":
     url = "https://example.com/"
     scan_url(url)
+import requests
+
+payloads = [
+    "<script>alert('XSS')</script>",
+    "><script>alert('XSS')</script><",
+    # ...
+]
+
+target_urls = [
+    "http://example.com/search?q=",
+    "http://example.com/profile?name=",
+    # ...
+]
+
+for target_url in target_urls:
+    for payload in payloads:
+        response = requests.get(target_url + payload)
+        if payload in response.text:
+            print(f"{target_url} is vulnerable to XSS using payload {payload}.")
+            break
+    else:
+        print(f"{target_url} is not vulnerable to XSS.")
